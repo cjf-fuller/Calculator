175ebdf8c3f5f92eb2ca7c4c6be89f2f57d6fd3e
cjf-fuller <caspar.fuller95@gmail.com>
2018-06-19 10:18:15 +0100

    Added a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const readline = require('readline-sync');
 console.log('Welcome to Caspar\'s Calculator!');
+
+console.log('Please enter first number:');
+const responsex = readline.prompt();
+var x = parseFloat(responsex)
+
+console.log('Please enter second number:');
+const responsey = readline.prompt();
+var y = parseFloat(responsey)
+
+var product = x*y
+console.log('The product of ' + x + ' and ' + y + ' is ' + product)
