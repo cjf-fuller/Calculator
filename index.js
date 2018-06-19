0adf1575c39fb620851da8ad1e4608f0d848bb14
cjf-fuller <caspar.fuller95@gmail.com>
2018-06-19 10:31:20 +0100

    If/Else operator selector

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const readline = require('readline-sync');
 console.log('Welcome to Caspar\'s Calculator!');
+console.log('-------------------');
+
+console.log('Please enter operation symbol (+-*/):');
+const operator = readline.prompt();
 
 console.log('Please enter first number:');
 const responsex = readline.prompt();
-var x = parseFloat(responsex)
+var x = parseFloat(responsex);
 
 console.log('Please enter second number:');
 const responsey = readline.prompt();
-var y = parseFloat(responsey)
+var y = parseFloat(responsey);
 
-var product = x*y
-console.log('The product of ' + x + ' and ' + y + ' is ' + product)
+if (operator == '+'){
+var ans = x+y;
+console.log(x + ' + ' + y + ' = ' + ans)
+}
+else if (operator == '-'){
+var ans = x-y;
+console.log(x + ' - ' + y + ' = ' + ans);
+
+}
+else if (operator == '*'){
+var ans = x*y;
+console.log(x + ' * ' + y + ' = ' + ans);
+
+}
+else if (operator == '/'){
+var ans = x/y;
+console.log(x + ' / ' + y + ' = ' + ans);
+
+}
+
+else {
+    console.log("ERROR - Please enter a valid operator.")
+}
+
+
