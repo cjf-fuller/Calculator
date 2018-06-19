fa5a5e16bce4c6e841709ba7b60533e083bfedba
cjf-fuller <caspar.fuller95@gmail.com>
2018-06-19 18:05:48 +0100

    Added Vowel Calculator

--- index.js
+++ index.js
@@ -1,50 +1,28 @@
 const readline = require('readline-sync');
 
+const ARITHMETIC_MODE = '1';
+const VOWEL_COUNTING_MODE = '2';
+
 printWelcome();
 while(true){
-    performCalculation();
+    const calculationMode = getCalculationMode();
+    if (calculationMode === 1) {
+        performOneArithmeticCalculation();
+    }
+    else if (calculationMode === 2){
+        performOneVowelCountingCalculation();
+    }
 }
 
 
-function performCalculation()
+function performOneArithmeticCalculation()
 {
 console.log('Please enter operation symbol (+-*/):');
 const operator = readline.prompt();
+console.log('Please enter the number of numbers you want to '+operator+':');
+var numTot = inputInt();
+var numArr = inputNumbers(numTot);
 
-var x = 0;
-do {
-    console.log('Please enter the number of numbers you want to '+operator+':');
-    const numTo = readline.prompt();
-    var numTot = parseInt(numTo);
-    if (isNaN(numTot)){
-         console.log("Please enter a valid input.")
-         continue;
-    }
-    else{
-        x = 1;
-    }
-
-}while (x===0);
-
-var numArr = [];
-for(i=0; i<numTot; i++){
-    x = 0;
-    do {
-        console.log('Please enter number ' + (i+1) +':')
-        var num = readline.prompt();
-        number = parseFloat(num);
-        if (isNaN(number)){
-            console.log("Please enter a valid input")
-            continue;
-        }
-        else{
-            numArr.push(number);
-            x = 1;
-        }
-    }while(x===0)
-}
-
-console.log(numArr)
 switch(operator) {
     case "+":
         var ans = numArr[0];
@@ -78,4 +56,86 @@ console.log("Answer = " + ans);
 function printWelcome(){
     console.log('Welcome to Caspar\'s Calculator!');
     console.log('-------------------');
+}
+
+function inputInt(){
+    var x = 0;
+    do {
+        const numTo = readline.prompt();
+        var numTot = parseInt(numTo);
+        if (isNaN(numTot)){
+             console.log("Please enter a valid input.")
+             continue;
+        }
+        else{
+            x = 1;
+        }
+    
+    }while (x===0);
+    return numTot
+}
+function inputNumbers(numTot){
+    var numArr = [];
+    for(i=0; i<numTot; i++){
+        x = 0;
+        do {
+            console.log('Please enter number ' + (i+1) +':')
+            var num = readline.prompt();
+            number = parseFloat(num);
+            if (isNaN(number)){
+                console.log("Please enter a valid input")
+                continue;
+            }
+            else{
+                numArr.push(number);
+                x = 1;
+            }
+        }while(x===0)
+    }
+    return numArr;
+}
+function getCalculationMode(){
+    console.log("Which calculation mode do you want to use?\n Please press 1 for arithmetic\n Please press 2 for vowel...");
+    var AOrV = inputInt();
+    if (AOrV === 1){
+        return 1;
+    }
+    else if (AOrV === 2){
+        return 2;
+    }
+    else {return Error}
+}
+function performOneVowelCountingCalculation(){
+    console.log("Please enter a string to be evaluated:");
+    var stringTC = readline.prompt();
+    stringTC = stringTC.toLowerCase();
+    
+    var vowelCount = {
+        'Acount': a=0,
+        'Ecount': e=0,
+        'Icount': i=0,
+        'Ocount': o=0,
+        'Ucount': u=0 
+    };
+    for (x=0; x< stringTC.length; x++ ){
+        if(stringTC[x]==='a'){
+            vowelCount.Acount++
+        }
+        else if(stringTC[x]==='e'){
+            vowelCount.Ecount++
+        }
+        else if(stringTC[x]==='i'){
+            vowelCount.Icount++
+        }
+        else if(stringTC[x]==='o'){
+            vowelCount.Ocount++
+        }
+        else if(stringTC[x]==='u'){
+            vowelCount.Ucount++
+        }
+        else{
+            continue;
+        }
+    }
+    console.log(vowelCount);
 }
